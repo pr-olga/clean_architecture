3a44110d24332846454fd5dd912062665db4982b
pr-olga <frkhorkovaolga@gmail.com>
2020-06-14 12:50:53 +0200

    feat: init

---
+++ dip/dip.js
@@ -0,0 +1,106 @@
+/**
+ * Class representing the buying process
+ */
+class Store {
+  /**
+   * Create Customer and Payment
+   * @param {string} customer
+   * @param {string} payment
+   */
+  constructor(customer, payment) {
+    this.customer = new Customer(customer)
+    this.payment = new PaymentProcessor(this.customer, payment)
+  }
+
+  /**
+   * Buy jeans
+   * @param {number} quantity
+   */
+  purchaseJeans(quantity) {
+    this.payment.pay(quantity)
+  }
+
+  /**
+   * Buy books
+   * @param {number} quantity
+   */
+  purchaseBooks(quantity) {
+    this.payment.pay(quantity)
+  }
+}
+
+/**
+ * Class representing customer
+ */
+class Customer {
+  constructor(name) {
+    this.name = name
+  }
+}
+
+/**
+ * Class representing Dependency Inversion Principle
+ * Abstraction Layer between buying and paying processes
+ */
+class PaymentProcessor {
+  /**
+   * Define customer and payment
+   * @param {string} customer
+   * @param {string} payment
+   */
+  constructor(customer, payment) {
+    this.customer = customer
+    this.payment = payment
+  }
+
+  /**
+   * Method representing Strategy Pattern for Payment
+   * @param {number} quantity
+   *
+   */
+  pay(quantity) {
+    let payment = null
+
+    switch (this.payment) {
+      case 'Stripe':
+        payment = new Stripe
+        break
+      case 'PayPal':
+        payment = new PayPal
+        break
+      case 'Cash':
+        payment = new Cash
+        break
+      default:
+        break
+    }
+
+    if (!payment) {
+      console.log('Error: Payment must be defined')
+      return
+    }
+
+    return payment.makePayment(this.customer, quantity)
+  }
+}
+
+class Stripe {
+  makePayment(user, quantity) {
+    console.log(`${user.name} paid for ${quantity} amounts by Stripe ($)`)
+  }
+}
+
+class PayPal {
+  makePayment(user, quantity) {
+    console.log(`${user.name} paid for ${quantity} amounts by PayPal (Euro)`)
+  }
+}
+
+class Cash {
+  makePayment(user, quantity) {
+    console.log(`${user.name} paid for ${quantity} amounts in cash (Rubles)`)
+  }
+}
+
+const buy = new Store('Olga', 'Stripe')
+buy.purchaseBooks(2)
